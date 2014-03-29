942ed261e6367cfa0098fe30d250d84d34847396
Alexander Cobleigh <angellus.falsus@gmail.com>
2014-03-29 16:58:26 +0100

    added less

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(require('less-middleware')(path.join(__dirname, 'public') ));
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
--- public/javascripts/bottle.js
+++ public/javascripts/bottle.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
-    console.log("bottle loaded");
+    log("bottle loaded");
     $("#throw").on("click", function() {
         var letterContents = $("#letter").val();
-        console.log("click");
         $.ajax({
             type: 'POST',
             data: letterContents,
             contentType: 'application/json',
             url: "http://localhost",	
             success: function(data) {
-                console.log('success!!!!');
-                console.log(letterContents);
+                log('success!!!!');
+                log(letterContents);
             }
         });
     });
+
+    function log(msg) {
+        console.log("[bottle] " + msg);
+    }
 });
