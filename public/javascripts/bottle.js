00b62e516b509d3c0fd1e3c0ea4d0021451f94f0
Alexander Cobleigh <angellus.falsus@gmail.com>
2014-03-30 00:38:22 +0100

    more ding and typewriter things

--- public/javascripts/bottle.js
+++ public/javascripts/bottle.js
@@ -1,4 +1,39 @@
 $(document).ready(function() {
+    var ns = 20; //The number of sounds to preload. This depends on how often the sounds need to be played, but if too big it will probably cause long loading times.
+    var sounds = []; //This will be a matrix of all the sounds
+    var sources = [];
+    sources.push("/sounds/keypress-shorter.ogg");    
+    sources.push("/sounds/ding-shorter.ogg");    
+    
+    //We need to have ns different copies of each sound, hence:
+    for (i = 0; i < ns; i ++) 
+        sounds.push([]);
+
+    for (i = 0; i < sources.length; i++) {
+        for (j = 0; j < ns ; j++) {
+            sounds[j].push(new Audio(sources[i]));
+        }
+    }
+    
+    var playing = [];
+    playing[0] = 0;
+    playing[1] = 0;
+
+
+    // vol is a real number in the [0, 1] interval
+    var playSound = function(id, vol) {
+        if (vol <= 1 && vol >= 0)
+            sounds[playing[id]][id].volume = vol;
+        else
+            sounds[playing[id]][id].volume = 1;
+
+        sounds[playing[id]][id].play();
+        ++playing[id]; //Each time a sound is played, increment this so the next time that sound needs to be played, we play a different version of it,
+
+        if (playing[id] >= ns)
+            playing[id] = 0;
+    }
+
     log("bottle loaded");
     $("#throw").on("click", function() {
         var letterContents = $("#letter").val();
@@ -28,6 +63,21 @@ $(document).ready(function() {
 
     $("#search").on("click", function() {
         log("searched for a bottle");
+    });
+
+
+    // naive line check, to prevent users from exceeding length of letter with
+    // newlines
+    // doesn't really work that well
+    var maxLines = 40;
+    $("#letter").keydown(function(event) {
+        var newLines = $(this).val().split("\n").length;
+
+        if(event.keyCode == 13) {
+            playSound(1);
+        } else {
+            playSound(0);
+        }
     });
 
     function log(msg) {
