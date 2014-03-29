705432861af956d68922b10fcc03c7552876324e
Alexander Cobleigh <angellus.falsus@gmail.com>
2014-03-29 16:23:18 +0100

    internauts++

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var internaut = require("./internaut-waves.js")
 
 
 
@@ -31,7 +32,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.post("/", internaut.post);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
